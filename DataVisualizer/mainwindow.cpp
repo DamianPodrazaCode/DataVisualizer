f904aef4234110be4ddd1d9bafafd032dde92743
Damian Podraza <damian.podraza@gmail.com>
2023-10-27 11:31:22 +0200

    Signed-off-by: Damian Podraza <damian.podraza@gmail.com>

--- DataVisualizer/mainwindow.cpp
+++ DataVisualizer/mainwindow.cpp
@@ -10,3 +10,8 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 MainWindow::~MainWindow() {
     delete ui;
 }
+
+void MainWindow::on_actionExit_triggered() {
+    QApplication::exit();
+}
+
--- DataVisualizer/mainwindow.h
+++ DataVisualizer/mainwindow.h
@@ -16,6 +16,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionExit_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
