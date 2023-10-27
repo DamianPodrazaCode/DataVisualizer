8e0829ebddd3df5b628d66a1f95520d86e52328e
Damian Podraza <damian.podraza@gmail.com>
2023-10-27 11:14:38 +0200

    Signed-off-by: Damian Podraza <damian.podraza@gmail.com>

--- DataVisualizer/mainwindow.cpp
+++ DataVisualizer/mainwindow.cpp
@@ -1,15 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-      , ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    QApplication::setApplicationName("Data Visualizer");
+    MainWindow::setWindowTitle("Data Visualizer");
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
-
